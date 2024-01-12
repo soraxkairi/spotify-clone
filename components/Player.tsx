2a316b1fc603026f5393ad14f9b288ae6b253cbe
soraxkairi <=>
2024-01-12 10:14:31 +0100

    Modificado fichero types_db

--- components/Player.tsx
+++ components/Player.tsx
@@ -19,16 +19,7 @@ const Player = () => {
 
   return (
     <div 
-      className="
-        fixed 
-        bottom-0 
-        bg-black 
-        w-full 
-        py-2 
-        h-[80px] 
-        px-4
-      "
-    >
+      className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
         {/* key destruye el elemento y renderiza el nuevo */}
       <PlayerContent key={songUrl} songUrl={songUrl} song={song}/>
     </div>
